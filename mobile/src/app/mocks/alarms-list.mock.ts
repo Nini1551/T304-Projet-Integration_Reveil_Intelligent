9597c262042651513a218a167e7779d2d54e0b55
Nini1551 <47613768+Nini1551@users.noreply.github.com>
2024-10-03 20:31:56 +0200

    refactor(alarmsList): export the mocked value from a new mocks directory. #6

---
+++ mobile/src/app/mocks/alarms-list.mock.ts
@@ -0,0 +1,7 @@
+import { Alarm } from "../interfaces/alarms";
+
+export const ALARMS_LIST_MOCK: Alarm[] = [
+  { id: 1, name: "Projet d'intégration", hour: 8, location: "EPHEC", ringtone: "Ringtone 1", prepTime: 10, active: true },
+  { id: 2, name: "Sécurité des réseaux (Théorie)", hour: 12, location: "EPHEC", ringtone: "Ringtone 2", prepTime: 5, active: false },
+  { id: 3, name: "Sécurité des réseaux (Pratique)", hour: 14, location: "EPHEC", ringtone: "Ringtone 3", prepTime: 15, active: true }
+];
